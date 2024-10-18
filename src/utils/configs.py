0d94b9e6a181817b4817721fae55d37e5494450b
Thiago Vilarinho Lemes <lemes_vilarinho@yahoo.com.br>
2024-10-18 10:54:24 -0300

    PPE-7 Criar pasta notebooks_testes e o script extracao_dados_censo_escolar.ipynb para exploração dos dados do Censo Escolar

--- src/utils/configs.py
+++ src/utils/configs.py
@@ -19,3 +19,19 @@ PASTA_UTILS = PASTA_SRC / "utils"
 
 # Subir para o diretório raiz (ajuste o número de '..' conforme necessário)
 # project_root = os.path.abspath(os.path.join(current_dir, '..'))
+
+# Nova forma de extrair os caminhos
+# try:
+#     PASTA_NOTEBOOK / ""
+# except:
+#     PASTA_NOTEBOOK = Path(os.getcwd())
+    
+# PASTA_PROJETO = PASTA_NOTEBOOK.parent
+# PASTA_DADOS = PASTA_PROJETO / "dados"
+# PASTA_EXTERNO = PASTA_DADOS / "externo"
+# PASTA_TESTE = PASTA_PROJETO / "src/tests"
+# PASTA_TESTE_CODIGO = PASTA_TESTE / "codigos"
+
+# PASTA_TESTE_CODIGO.mkdir(exist_ok=True)
+
+# os.chdir(PASTA_PROJETO)
